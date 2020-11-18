7e084f87d36d8b89b8fbca4f2f72bd8937dac4f2
Filipe Astolfi <72992937+FilipeAstolfi@users.noreply.github.com>
2020-11-18 20:05:08 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,37 @@
+function exibir_categoria(categoria){
+    //alert (categoria);
+
+    let elementos = document.getElementsByClassName('boxproduto');
+    console.log(elementos);
+    for(var i=0; i<elementos.length; i++){
+        console.log(elementos[i].id);
+        if(categoria == elementos[i].id)
+        elementos[i].style = "display:inline-block";
+        else elementos[i].style  = "display:none";
+    }
+}
+
+let exibir_todos = () => {
+    let elementos = document.getElementsByClassName('boxproduto');
+
+    for(var i=0; i<elementos.length; i++){
+        elementos[i].style = "display:inline-block";
+    }
+};
+
+let destaque = (imagem) => {
+    console.log(imagem);
+    if(imagem.width == 240) 
+        imagem.width = 120;
+    else
+        imagem.width = 240;
+};
+
+function mudaCor(obj) {
+    if (obj.style.color == 'red') {
+        obj.style.color = 'black';
+
+    } else {
+        obj.style.color = 'red';
+    }
+}
